7bf541a6867a09a6d6af90937cf79225b6d0ccc7
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-09-26 11:25:49 -0400

    style session card

--- src/components/exam/SessionCard.tsx
+++ src/components/exam/SessionCard.tsx
@@ -17,58 +17,50 @@ const SessionCard: React.FC<SessionCardProps> = (props) => {
     shuffledAnswerBank,
     isMultipleChoice,
   } = props.question;
+  // Consolidate Input components from two to one
   return (
     <>
-      <h4>Question {props.currentQuestion}:</h4>
+      <p>Question {props.currentQuestion}:</p>
       <p>{question}</p>
-      <ul>
-        <Form id="exam-form">
-          {shuffledAnswerBank.map((answer) => {
-            return (
-              <li key={answer.answerId}>
-                <FormGroup check>
-                  <Label check>
-                    {isMultipleChoice ? (
-                      <Input
-                        type="radio"
-                        name="radio1"
-                        onChange={(event) =>
-                          props.answerSelect(event, questionId, answer.answerId)
-                        }
-                        value={answer.answerId}
-                      />
-                    ) : (
-                      <Input
-                        type="checkbox"
-                        name="checkbox1"
-                        onChange={(event) =>
-                          props.answerSelect(event, questionId, answer.answerId)
-                        }
-                        value={answer.answerId}
-                      />
-                    )}
-                    {answer.choice}
-                  </Label>
-                </FormGroup>
-              </li>
-            );
-          })}
-          <Button
-            size="lg"
-            color="primary"
-            onClick={(e) => props.nextQuestion(e)}
-          >
-            {/* {userAnswers![
-            examData.questions[
-              examData.currentQuestion - 1
-            ].questionId.toString()
-          ].answers.length
-            ? "Next Question"
-            : "Skip Question"} */}
-            Next Question
-          </Button>
-        </Form>
-      </ul>
+
+      <Form id="exam-form">
+        {shuffledAnswerBank.map((answer) => {
+          return (
+            <FormGroup check key={answer.answerId}>
+              <Label check>
+                {isMultipleChoice ? (
+                  <Input
+                    type="radio"
+                    name="radio1"
+                    onChange={(event) =>
+                      props.answerSelect(event, questionId, answer.answerId)
+                    }
+                    value={answer.answerId}
+                  />
+                ) : (
+                  <Input
+                    type="checkbox"
+                    name="checkbox1"
+                    onChange={(event) =>
+                      props.answerSelect(event, questionId, answer.answerId)
+                    }
+                    value={answer.answerId}
+                  />
+                )}
+                {answer.choice}
+              </Label>
+            </FormGroup>
+          );
+        })}
+        <Button
+          size="lg"
+          color="primary"
+          className="actionButton"
+          onClick={(e) => props.nextQuestion(e)}
+        >
+          {props.currentQuestion === 6 ? "Finish Exam" : "Next Question"}
+        </Button>
+      </Form>
     </>
   );
 };
