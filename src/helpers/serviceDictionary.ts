dfbff7eff1bc7fbb5ee237178248fb26125d4b03
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-10-18 00:24:29 -0400

    add first exam

---
+++ src/helpers/serviceDictionary.ts
@@ -0,0 +1,8 @@
+export const serviceDictionary = {
+  "AWS Peronsal Health Dashboard": `AWS Personal Health Dashboard provides alerts and remediation guidance when AWS is experiencing events that may impact you. While the Service Health Dashboard displays the general status of AWS services, Personal Health Dashboard gives you a personalized view into the performance and availability of the AWS services underlying your AWS resources.`,
+  "AWS Support API": `The AWS Support API provides access to some of the features of the AWS Support Center via an API.`,
+  "AWS Concierge Service": `AWS Support Concierge Service assists customers with account and billing inquiries.`,
+  "Amazon Simple Email Service": `Amazon Simple Email Service (Amazon SES) is a cloud-based email sending service designed to help digital marketers and application developers send marketing, notification, and transactional emails.`,
+  "Amazon Connect": `Amazon Connect is a self-service, cloud-based contact center service that makes it easy for any business to deliver better customer service at lower cost.`,
+  "AWS CloudTrail": `AWS CloudTrail is a service that enables governance, compliance, operational auditing, and risk auditing of your AWS account.`,
+};
