e43f7fb93a9b07d7f80d6b5d340b0ba14ee44843
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-11-22 19:36:59 -0500

    created countdowntimer and controlsmodal component

---
+++ src/components/countdownTimer/CountdownTimer.js
@@ -0,0 +1,15 @@
+import { divide } from "lodash";
+import React from "react";
+
+const CountdownTimer = () => {
+  return (
+    <div>
+      <div>Icon</div>
+      <div>Timer</div>
+      <div>Pause Icon</div>
+      <div>Stop Icon</div>
+    </div>
+  );
+};
+
+export default CountdownTimer;
---
+++ src/components/modal/ControlsModal.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ControlsModal = () => {
+  return <div>controls modal</div>;
+};
+
+export default ControlsModal;
