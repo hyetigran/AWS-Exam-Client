43a5dc9536ea5abe3ed1a5847d764bb407e495ba
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-12-20 11:58:02 -0500

    add pie chart to review page and add option dropdown component

--- src/components/reviewPaper/reviewPaper.tsx
+++ src/components/reviewPaper/reviewPaper.tsx
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { PieChart } from "react-minimal-pie-chart";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 import { useHistory } from "react-router-dom";
+
+import "./ReviewPaper.css";
 
 type ReviewPaperProps = {};
 
 const ReviewPaper: React.FC<ReviewPaperProps> = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("All Questions");
+  let dropdownOptions = ["All Questions", "Correct", "Incorrect", "Skipped"];
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+
   return (
-    <div>
-      <div>options</div>
+    <div className="review-paper">
+      <div className="review-overview">
+        <div className="review-chart">
+          <PieChart
+            className="thumb-piechart"
+            data={[
+              { title: "Correct", value: 1, color: "#85edc2" },
+              { title: "Incorrect", value: 65 - 1, color: "#ff7372" },
+            ]}
+            radius={PieChart.defaultProps.radius - 10}
+            lineWidth={15}
+            segmentsStyle={{ transition: "stroke .3s", cursor: "pointer" }}
+            // labelPosition={100 - lineWidth / 2}
+            labelStyle={{
+              fill: "#fff",
+              opacity: 0.75,
+              pointerEvents: "none",
+            }}
+          />
+        </div>
+        <div className="review-dropdown">
+          <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+            <DropdownToggle caret>{selectedOption}</DropdownToggle>
+            <DropdownMenu>
+              {dropdownOptions.map((opt) => (
+                <DropdownItem key={opt} onClick={() => setSelectedOption(opt)}>
+                  {opt}
+                </DropdownItem>
+              ))}
+            </DropdownMenu>
+          </Dropdown>
+        </div>
+      </div>
       <div>review</div>
     </div>
   );
--- src/views/ExamReview/ExamReview.tsx
+++ src/views/ExamReview/ExamReview.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../store/history/types";
-
-import { thunkFetchQuestionHistory } from "../../store/history/actions";
 import { useHistory, useParams } from "react-router-dom";
+import { Button } from "reactstrap";
+
+import { RootState } from "../../store/history/types";
+import { thunkFetchQuestionHistory } from "../../store/history/actions";
+import ReviewPaper from "../../components/reviewPaper/ReviewPaper";
+import "./ExamReview.css";
 
 interface ReviewParams {
   examType: string;
@@ -18,6 +21,8 @@ const ExamReview = () => {
   let isFromHistory = history.location.state?.from === "history" ? true : false;
   let exam = isFromHistory && history.location.state.exam;
 
+  let { examType, examNumber } = examReviewData[0];
+
   useEffect(() => {
     fetchFullExamSession();
   }, []);
@@ -26,7 +31,19 @@ const ExamReview = () => {
     dispatch(thunkFetchQuestionHistory(examSessionID, isFromHistory, exam));
   };
   console.log(examReviewData);
-  return <div>ExamReviewComponent</div>;
+  return (
+    <div>
+      <div className="review-title">
+        <h3>{`AWS ${examType} Practice Exam (${examNumber})`}</h3>
+      </div>
+      <div className="review-link">
+        <Button color="link" onClick={() => history.push("/exam-history")}>
+          {"< Return to history"}
+        </Button>
+      </div>
+      <ReviewPaper />
+    </div>
+  );
 };
 
 export default ExamReview;
